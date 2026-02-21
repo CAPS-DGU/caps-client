9a2cfa6207826a629f2e19f3ef3dbb74358b9d46
dvin-22 <dain5835@gmail.com>
2026-02-21 18:10:21 +0900

    fix: 26.02 기준 집행부원 업데이트

--- src/data/aboutUs/departments/editorial.ts
+++ src/data/aboutUs/departments/editorial.ts
@@ -9,21 +9,13 @@ const department: Department = {
       img: "김승우.jpeg",
     },
     {
-      name: "39기 강성찬",
-      position: "컴퓨터AI학부",
-      img: "강성찬.jpeg",
-    },
-    {
-      name: "39기 윤민재",
-      position: "컴퓨터AI학부",
-      img: "윤민재.jpeg",
-    },
-    {
       name: "39.5기 김예은",
       position: "컴퓨터AI학부",
       img: "김예은.jpeg",
     },
-    { name: "39.5기 허윤", position: "컴퓨터AI학부", img: "허윤.jpeg" },
+    { name: "39.5기 허윤", 
+      position: "컴퓨터AI학부", 
+      img: "허윤.jpeg" },
   ],
 };
 
--- src/data/aboutUs/executives.ts
+++ src/data/aboutUs/executives.ts
@@ -3,15 +3,15 @@ import type { Executive } from "../../types/aboutUs";
 const executives: Executive[] = [
   {
     role: "회장",
-    name: "37기 신효환",
+    name: "39기 김영모",
     position: "컴퓨터AI학부",
-    img: "신효환.jpeg",
+    img: "김영모.jpeg",
   },
   {
     role: "부회장",
-    name: "38기 성준영",
-    position: "수학과",
-    img: "성준영.jpeg",
+    name: "39기 강지원",
+    position: "에너지신소재공학과",
+    img: "강지원.jpeg",
   },
 ];
 

6768a96eb64e8c435f3f97d6b9f203d1773bb610
dvin-22 <154444746+dvin-22@users.noreply.github.com>
2026-08-13 20:19:38 +0900

    docs: 집행부 소개 페이지 순서 변경

--- src/data/aboutUs/departments/planning.ts
+++ src/data/aboutUs/departments/planning.ts
@@ -9,14 +9,14 @@ const department: Department = {
       img: "정율.jpeg",
     },
     {
-      name: "38.5기 노혜륜",
-      position: "컴퓨터AI학부",
-      img: "노혜륜.jpeg",
-    },
-    {
       name: "37기 김동원",
       position: "멀티미디어소프트웨어공학전공",
       img: "김동원.jpeg",
+    },
+    {
+      name: "38.5기 노혜륜",
+      position: "컴퓨터AI학부",
+      img: "노혜륜.jpeg",
     },
     {
       name: "39기 정형균",
